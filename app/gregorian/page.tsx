550ecb506275b25c4d2529a4a7ad6cdb94a60e28
mohammad <mohammadkarimi.swctw@gmail.com>
2025-02-18 21:15:52 +0330

    enhance gregorian layout page

--- app/gregorian/page.tsx
+++ app/gregorian/page.tsx
@@ -6,20 +6,24 @@ import { gregorianFormatWeekdayName } from "@/lib/calendar";
 
 export default function GregorianCalendarPage() {
   return (
-    <div className="flex flex-col gap-5 z-10 mb-36" dir="ltr" lang="en">
-      <h3>Gregorian Calendar</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-5"></div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
-        <CalendarGregorian mode="single" />
-        <div className="w-[400px]">
-          <CalendarGregorian
-            mode="single"
-            formatters={{ formatWeekdayName: gregorianFormatWeekdayName }}
-          />
+    <div className="flex flex-col gap-12 z-10 mb-36" dir="ltr" lang="en">
+      <div className="space-y-5">
+        <h3>Gregorian Calendar</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
+          <CalendarGregorian mode="single" />
+          <div className="w-[400px]">
+            <CalendarGregorian
+              mode="single"
+              formatters={{ formatWeekdayName: gregorianFormatWeekdayName }}
+            />
+          </div>
         </div>
       </div>
-      <br />
-      <h3>Advanced Calenders</h3>
+
+      <div className="space-y-5">
+        <h3>Advanced Calenders</h3>
+      </div>
     </div>
   );
 }
