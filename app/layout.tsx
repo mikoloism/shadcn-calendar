2e78628d07fe154c9166146768d969d0a0852e2d
mohammad <mohammadkarimi.swctw@gmail.com>
2025-02-18 20:45:38 +0330

    enhance layout structure by add Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { iranSans, iranSansFaNum } from "./fonts";
+import Header from "@/components/Header";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,9 +16,12 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body
-        className={`${iranSansFaNum.className}, ${iranSans.className} antialiased`}
+        className={`${iranSansFaNum.className} ${iranSans.className} antialiased`}
       >
-        {children}
+        <div className="px-3 sm:px-8 md:px-16 lg:px-22 py-4 grid gap-y-8">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
