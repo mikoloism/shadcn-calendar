e5e97e389a42f8545f23808024af0bd47386a084
mohammad <mohammadkarimi.swctw@gmail.com>
2025-02-27 20:44:45 +0330

    wrap pages with LayoutWrapper

--- app/gregorian/page.tsx
+++ app/gregorian/page.tsx
@@ -5,31 +5,34 @@ import { CalendarGregorian } from "@/components/calendar/calendar-gregorian";
 import { gregorianFormatWeekdayName } from "@/lib/calendar";
 import { DropdownPickerGregorian } from "@/components/calendar/examples/dropdown-picker-gregorian";
 import { BookedPickerGregorian } from "@/components/calendar/examples/booked-picker-gregorian";
+import LayoutWrapper from "@/components/LayoutWrapper";
 
 export default function GregorianCalendarPage() {
   return (
-    <div className="flex flex-col gap-12 z-10 mb-36" dir="ltr" lang="en">
-      <div className="space-y-5">
-        <h3>Gregorian Calendars</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
-          <CalendarGregorian mode="single" />
-          <div className="w-[400px]">
-            <CalendarGregorian
-              mode="single"
-              formatters={{ formatWeekdayName: gregorianFormatWeekdayName }}
-            />
+    <LayoutWrapper lang="en" dir="lrt">
+      <div className="flex flex-col gap-12 z-10 mb-36">
+        <div className="space-y-5">
+          <h3>Gregorian Calendars</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
+            <CalendarGregorian mode="single" />
+            <div className="w-[400px]">
+              <CalendarGregorian
+                mode="single"
+                formatters={{ formatWeekdayName: gregorianFormatWeekdayName }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-5">
+          <h3>Advanced Calenders</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DropdownPickerGregorian />
+            <BookedPickerGregorian />
           </div>
         </div>
       </div>
-
-      <div className="space-y-5">
-        <h3>Advanced Calenders</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          <DropdownPickerGregorian />
-          <BookedPickerGregorian />
-        </div>
-      </div>
-    </div>
+    </LayoutWrapper>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,34 +8,37 @@ import { MultiplePickerJalali } from "@/components/calendar/examples/multiple-pi
 import { jalaliFormatWeekdayName } from "@/lib/calendar";
 import { DropdownPickerJalali } from "@/components/calendar/examples/dropdown-picker-jalali";
 import { BookedPickerJalali } from "@/components/calendar/examples/booked-picker-jalali";
+import LayoutWrapper from "@/components/LayoutWrapper";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-12 z-10 mb-36">
-      <div className="space-y-5">
-        <h3>تقویم های جلالی</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          <DatePickerJalali />
-          <RangePickerJalali />
-          <MultiplePickerJalali />
+    <LayoutWrapper>
+      <div className="flex flex-col gap-12 z-10 mb-36">
+        <div className="space-y-5">
+          <h3>تقویم های جلالی</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DatePickerJalali />
+            <RangePickerJalali />
+            <MultiplePickerJalali />
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
+            <CalendarJalali mode="single" />
+            <div className="w-[400px]">
+              <CalendarJalali
+                mode="single"
+                formatters={{ formatWeekdayName: jalaliFormatWeekdayName }}
+              />
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
-          <CalendarJalali mode="single" />
-          <div className="w-[400px]">
-            <CalendarJalali
-              mode="single"
-              formatters={{ formatWeekdayName: jalaliFormatWeekdayName }}
-            />
+        <div className="space-y-5">
+          <h3>تقویم های پیشرفته</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DropdownPickerJalali />
+            <BookedPickerJalali />
           </div>
         </div>
       </div>
-      <div className="space-y-5">
-        <h3>تقویم های پیشرفته</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          <DropdownPickerJalali />
-          <BookedPickerJalali />
-        </div>
-      </div>
-    </div>
+    </LayoutWrapper>
   );
 }
