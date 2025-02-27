8552072cc445120877ac1f0f68a5a969abcc3ed6
mohammad <mohammadkarimi.swctw@gmail.com>
2025-02-27 20:10:12 +0330

    correct header title in Gregorian calendar page

--- app/gregorian/page.tsx
+++ app/gregorian/page.tsx
@@ -10,7 +10,7 @@ export default function GregorianCalendarPage() {
   return (
     <div className="flex flex-col gap-12 z-10 mb-36" dir="ltr" lang="en">
       <div className="space-y-5">
-        <h3>Gregorian Calendar</h3>
+        <h3>Gregorian Calendars</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-5">
           <CalendarGregorian mode="single" />
