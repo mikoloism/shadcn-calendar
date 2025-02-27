eb6ca3a94b5115af762db2b613700c08dcd0bed1
mohammad <mohammadkarimi.swctw@gmail.com>
2025-02-27 20:43:59 +0330

    simplify RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-import type { Metadata } from "next";
-import { iranSans, iranSansFaNum } from "./fonts";
-import Header from "@/components/Header";
-import "./globals.css";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Shadcnui Jalali Date Picker",
@@ -10,19 +7,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <html lang="fa" dir="rtl">
-      <body
-        className={`${iranSansFaNum.className} ${iranSans.className} antialiased`}
-      >
-        <div className="px-3 sm:px-8 md:px-16 lg:px-22 py-4 grid gap-y-8">
-          <Header />
-          {children}
-        </div>
-      </body>
-    </html>
-  );
+}) {
+  return <>{children}</>;
 }
