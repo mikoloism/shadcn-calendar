e8d70c4493b37a8d22ed02445b95c3832e2e0666
mikoloism <mikoloism@gmail.com>
2025-04-28 19:49:52 +0330

    feat(calendar/mod): barrel exporting Calendar, GregorianCalendar, and PersianCalendar components

---
+++ components/calendar/mod.ts
@@ -0,0 +1,4 @@
+export { Calendar, Calendar as default, type CalendarProps } from "./Calendar";
+export { GregorianCalendar, type GregorianCalendarProps } from "./GregorianCalendar";
+export { PersianCalendar, type PersianCalendarProps } from "./PersianCalendar";
+
